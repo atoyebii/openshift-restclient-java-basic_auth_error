63a05816d69f1495226480a7104d20c804106f03
Andre Dietisheim <adietish@redhat.com>
2020-01-28 12:32:10 +0100

    initial commit

---
+++ src/com/openshift/restclient/java/test/AuthTest.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc. Distributed under license by Red Hat, Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.openshift.restclient.java.test;
+
+import org.junit.Test;
+
+import com.openshift.restclient.ClientBuilder;
+import com.openshift.restclient.IClient;
+import com.openshift.restclient.ResourceKind;
+import com.openshift.restclient.model.template.ITemplate;
+
+public class AuthTest {
+
+	@Test
+    public void shouldStackOverflowWhenBadPw() {
+        IClient client = new ClientBuilder("https://192.168.64.171:8443")
+                .withUserName("developer")
+                .withPassword("badpw")
+                .build();
+        ITemplate template = client.get(ResourceKind.TEMPLATE, "eap64-basic-s2i", "mynamespace");
+        System.out.println(template);
+    }
+
+}
